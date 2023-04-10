f5dccaab67a4df530960171aeba5bd54b112d275
Daniele Della Cioppa <daniele.cioppa@akhter.co.uk>
2023-04-10 20:27:42 +0100

    fic

--- app/src/main/java/com/example/simplelauncher/AppDrawerActivity.java
+++ app/src/main/java/com/example/simplelauncher/AppDrawerActivity.java
@@ -8,10 +8,12 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class AppDrawerActivity extends AppCompatActivity {
 
@@ -38,26 +40,61 @@ public class AppDrawerActivity extends AppCompatActivity {
 //        recyclerView.addItemDecoration(new GridSpacingItemDecoration(spanCount, spacing, includeEdge));
     }
 
-    private List getAppInfoList() {
-        List<AppInfo> appList = new ArrayList<>();
-        PackageManager packageManager = getPackageManager();
-        Intent intent = new Intent(Intent.ACTION_MAIN, null);
-        intent.addCategory(Intent.CATEGORY_LAUNCHER);
-        List resolveInfoList = packageManager.queryIntentActivities(intent, 0);
-//        for (ResolveInfo resolveInfo : resolveInfoList) {
+//    private List getAppInfoList() {
+//        List<AppInfo> appList = new ArrayList<>();
+//        PackageManager packageManager = getPackageManager();
+//        Intent intent = new Intent(Intent.ACTION_MAIN, null);
+//        intent.addCategory(Intent.CATEGORY_LAUNCHER);
+//        List resolveInfoList = packageManager.queryIntentActivities(intent, 0);
+//
+//        IntStream.range(0, resolveInfoList.size()).forEach(i -> {
 //            AppInfo appInfo = new AppInfo();
-//            appInfo.label = resolveInfo.loadLabel(packageManager).toString();
-//            appInfo.packageName = resolveInfo.activityInfo.packageName;
-//            appInfo.icon = resolveInfo.loadIcon(packageManager);
+//            appInfo.setAppName(((ResolveInfo) resolveInfoList.get(i)).loadLabel(packageManager).toString());
+//            appInfo.setPackageName(((ResolveInfo) resolveInfoList.get(i)).activityInfo.packageName);
+//            appInfo.setAppIcon(((ResolveInfo) resolveInfoList.get(i)).loadIcon(packageManager));
 //            appList.add(appInfo);
-//        }
-        for (int i = 0; i < resolveInfoList.size(); i++) {
+//        });
+//        return appList;
+//    }
+private List getAppInfoList()
+{
+    List appList = new ArrayList<>();
+    Intent intent = new Intent(Intent.ACTION_MAIN, null);
+    intent.addCategory(Intent.CATEGORY_LAUNCHER);
+    PackageManager packageManager = getPackageManager();
+    try
+    {
+        List<ResolveInfo> resolveInfoList = packageManager.queryIntentActivities(intent, 0);
+
+        for (ResolveInfo resolveInfo : resolveInfoList)
+        {
             AppInfo appInfo = new AppInfo();
-            appInfo.setAppName(((ResolveInfo) resolveInfoList.get(i)).loadLabel(packageManager).toString());
-            appInfo.setPackageName(((ResolveInfo) resolveInfoList.get(i)).activityInfo.packageName);
-            appInfo.setAppIcon(((ResolveInfo) resolveInfoList.get(i)).loadIcon(packageManager));
+            CharSequence label = resolveInfo.loadLabel(packageManager);
+            if (label != null)
+            {
+                appInfo.setAppName(label.toString());
+            }
+            appInfo.setPackageName(resolveInfo.activityInfo.packageName);
+            Drawable icon = resolveInfo.loadIcon(packageManager);
+            if (icon != null)
+            {
+                appInfo.setAppIcon(icon);
+            }
             appList.add(appInfo);
         }
-        return appList;
     }
+    catch (Exception e)
+    {
+        e.printStackTrace();
+    }
+    finally
+    {
+        if (packageManager != null)
+        {
+            packageManager = null;
+        }
+    }
+    return appList;
+}
+
 }
